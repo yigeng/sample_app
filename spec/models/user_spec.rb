a6f253b5e423bfa8ed4289574af5c377c52e29dd
yixuan <gengyixuan@me.com>
2013-04-21 13:27:35 -0700

    add annotate to Gemfile

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+describe User do
+
+  before { @user = User.new(name: "Example User", email: "user@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+end
